39cd643eee09d3c0d78b0f5f6d245987e980c20e
Frederico02 <81171707+Frederico02@users.noreply.github.com>
2024-04-08 10:37:29 -0300

    Update consulte.sh

--- consulte.sh
+++ consulte.sh
@@ -1,11 +1,18 @@
+# Solicita ao usuário o nome do usuário do Active Directory
+$userName = Read-Host "Digite o nome do usuário do Active Directory"
+
 # Tenta encontrar o usuário no Active Directory
-$user = Get-ADUser -Filter { SamAccountName -eq $userName }
+$user = Get-ADUser -Filter { SamAccountName -eq $userName } -Properties extensionAttribute1
 
 if ($user -eq $null) {
     Write-Host "Usuário não encontrado."
 } else {
-    # Obtém o valor atual de extensionAttribute1
-    $currentValue = $user.extensionAttribute1
+    # Obtém o valor atual do extensionAttribute1 do usuário
+    $extensionAttribute1Value = $user.extensionAttribute1
     
-    Write-Host "O valor atual do extensionAttribute1 para o usuário '$userName' é '$currentValue'."
+    if ($extensionAttribute1Value -ne $null) {
+        Write-Host "O valor do extensionAttribute1 para o usuário '$userName' é '$extensionAttribute1Value'."
+    } else {
+        Write-Host "O usuário '$userName' não possui um valor definido para extensionAttribute1."
+    }
 }
